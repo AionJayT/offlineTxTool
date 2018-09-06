d111fd86ba31b62e8d9bd5ea6d4f949d8e868b7e
jayTseng <jay.tseng@aion.network>
2018-09-06 14:35:29 -0400

    add testcase

--- test/org/aion/tool/OfflineSignTest.java
+++ test/org/aion/tool/OfflineSignTest.java
@@ -24,6 +24,11 @@
  */
 package org.aion.tool;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.security.interfaces.ECKey;
+import org.aion.base.util.ByteUtil;
 import org.aion.tool.KeyGenerator.KeyPair;
 import org.aion.tool.OfflineSignTx.SignedTx;
 import org.junit.Test;
@@ -42,11 +47,13 @@ public class OfflineSignTest {
         System.out.println("sec key2:" + kp2.getSecKey());
         System.out.println("address2:" + kp2.getAddr());
 
+        String from = "a0ddf224c387720e65058e8434a6012006ebe0756609e29129c5d0b601dca15d";
+        String sk = "ad03e484fa9db5d0cc6a21545ca2d08562adab480a5dbdeaddb8c2ac5615a34168055445065a7f33b0899670810fc849e61545b12427a2254f2d9cc09b9316b0";
+
         //Assign timestamp
-        String ts = "1";
         SignedTx stx = OfflineSignTx
-            .signedTx(kp.getAddr(), kp2.getAddr(), "1", "0", "", "50000", "10000000000",
-                kp.getSecKey(), ts);
+            .signedTx(from, kp2.getAddr(), "1", "14", "", "21000", "10000000000",
+                sk);
 
         System.out.println();
         System.out.println("SignedTx raw:" + stx.getRawTx());
@@ -61,5 +68,33 @@ public class OfflineSignTest {
         System.out.println("SignedTx raw:" + stx.getRawTx());
         System.out.println("SignedTx hash:" + stx.getMsgHash());
 
+    }
+
+    @Test
+    public void rawTxWithoutSign() {
+
+        KeyPair kp = KeyGenerator.genKey();
+        System.out.println("pub key:" + kp.getPubKey());
+        System.out.println("sec key:" + kp.getSecKey());
+        System.out.println("address:" + kp.getAddr());
+
+        String from = kp.getAddr();
+        String to = "a0ddf224c387720e65058e8434a6012006ebe0756609e29129c5d0b601dca15e";
+
+        byte[] res = OfflineSignTx.rawTxWithoutSign(from, to, "1", "14", "", 21000, 10000000000L);
+        assertNotNull(res);
+
+        System.out.println("unsigned rawdata: " + ByteUtil.toHexString(res));
+
+        // sign transaction
+        SignedTx stx = OfflineSignTx.signRawTx(res, kp.getSecKey(), 0);
+
+        System.out.println();
+        System.out.println("SignedTx raw:" + stx.getRawTx());
+        System.out.println("SignedTx hash:" + stx.getMsgHash());
+
+        AionTransaction tx = new AionTransaction(ByteUtil.hexStringToBytes(stx.getRawTx()));
+        tx.rlpParse();
+        assertArrayEquals(tx.getEncoded(), ByteUtil.hexStringToBytes(stx.getRawTx()));
     }
 }
